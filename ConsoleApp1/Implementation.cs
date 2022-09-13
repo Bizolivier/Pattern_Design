b9787523a4e6734fa9c6d51b83bbd7c0332c7d63
Bizolivier <bizimunguolivier3108@gmail.com>
2022-09-13 13:52:35 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Implementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton {
+    public class Logger {
+
+        private static readonly Lazy<Logger> _lazyLogger
+            = new Lazy<Logger>(() => new Logger());
+
+        /// <summary>
+        /// <see cref="Instance"/> 
+        /// </summary>
+        public static Logger Instance {
+            get {
+                return _lazyLogger.Value;
+            }
+
+        }
+        protected Logger() {
+        }
+
+        public void Log(string message) {
+            Console.WriteLine($"message to log {message}");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Singleton
+    {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Title = "Singleton parttern"; 
+            //call the property getter twice 
+            var instance1 = Logger.Instance;
+            var instance2 = Logger.Instance;
+
+            if (instance1 == instance2 && instance2 == Logger.Instance) {
+                Console.WriteLine("instance are the same.");
+
+            }
+            instance1.Log($"Message from {nameof(instance1)}");
+            instance1.Log($"Message from {nameof(instance2)}");
+            Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+            
+
+        }
+    }
+}
