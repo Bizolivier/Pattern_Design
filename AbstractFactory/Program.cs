53fa00aa4ee214344739bd9ade3fb2c6c4a662a4
Bizolivier <bizimunguolivier3108@gmail.com>
2022-09-27 13:54:23 +0200

    Abstract factory

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿using AbstractFactory;
+using System;
+
+Console.Title = "Abstract Factory";
+
+var belgiumShoppingCartPurchaseFactory = new BelgiumShoppingCartPurchaseFactory();
+var shoppingCartForBelgium = new ShoppingCart(belgiumShoppingCartPurchaseFactory);
+shoppingCartForBelgium.CalculeCosts();
+
+var franceShoppingCartPurchaseFactory = new FranceShoppingCartPurchaseFactory();
+var shoppingCartForFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+shoppingCartForFrance.CalculeCosts();
+
+Console.ReadKey();
+
+
+
